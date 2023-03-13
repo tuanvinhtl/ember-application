6a1fce82cb09822caca12e57b326bb15ea3cff91
vinh.nguyentuan <tuanvinhtl@gmail.com>
2023-03-13 22:09:28 +0700

    intergration success taiwdind and merged fontfamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,36 @@
 module.exports = {
   content: ['./app/**/*.{hbs,html,js}'],
   theme: {
-    extend: {},
+    colors: {
+      primary: 'var(--primary)',
+      'p—light': 'var(--p—light)',
+      'p—dark': 'var(--p—dark)',
+      secondary: 'var(--secondary)',
+      's—light': 'var(--s—light)',
+      's—dark': 'var(--s—dark)',
+      'p-text': 'var(--p-text)',
+      's-text': 'var(--s-text)',
+    },
+    fontFamily: {
+      roboto: ['Roboto', 'serif'],
+    },
+    fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    },
   },
-  plugins: [],
 };
